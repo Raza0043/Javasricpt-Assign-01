835e71ff8f700dd10b46d003828c1ff3d75adad6
Muhammad Ali Raza <155912766+Raza0043@users.noreply.github.com>
2024-08-25 01:35:41 +0500

    Add files via upload

---
+++ Assign #01/Ques#01/app.js
@@ -0,0 +1,37 @@
+// Assig no 01
+
+// Question no 01
+ 
+
+// var username 
+
+//  Question no 02
+// var myName =" Muhammad Ali raza"
+
+
+//  Question no 03
+
+// var message="Hello World"
+// alert(message)
+
+//  Question no 04
+
+// var stdname ="Ali Raza"
+// alert(stdname)
+// var years ="20 Years old"
+// alert(years)
+// var occupt ="Certified Data Engineer at Google"
+// alert(occupt)
+
+//  Question no 05
+// var dish ="PIZZA\nPIZZ\nPIZ\nPIZ\nPI\nP"
+// alert(dish)
+
+
+//  Question no 06
+// var email =" My Ema)il Address is alirazasiddiqui135@gamil.com"
+// alert(  email
+//  Question no 07
+// var book ="I am trying to learn from the book A Smarter Way to leran Javascript "
+// alert(book)
+//  Question no 08
---
+++ Assign #01/Ques#02/app.js
@@ -0,0 +1,24 @@
+// Assig no 01
+
+// Question no 01
+
+
+// alert("Hello WORLD")
+
+// Question no 02
+
+// alert("Error! Please enetr a valid input")
+
+// Question no 03
+// alert("Welcome to JS Land\n Happing Coding!!!")
+
+// Question no 04
+
+
+// alert("Welcome to JS Land")
+// alert(" Happing Coding!!!")
+
+// Question no 05
+// alert("Hello I can run JS Console through my web browser!!!")
+
+// Question no 0 6
